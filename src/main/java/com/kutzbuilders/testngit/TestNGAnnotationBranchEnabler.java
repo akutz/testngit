136ba809d869c8a9cd1ac6506d11f8cc29e9be52
akutz <sakutz@gmail.com>
2013-07-23 08:39:09 -0500

    logs now include the name of the executing test

--- src/main/java/com/kutzbuilders/testngit/TestNGAnnotationBranchEnabler.java
+++ src/main/java/com/kutzbuilders/testngit/TestNGAnnotationBranchEnabler.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
@@ -30,6 +31,15 @@ import org.testng.annotations.ITestAnnotation;
  * by separating them with a comma.
  * </p>
  * 
+ * <p>
+ * Additionally, in the case of a build environment where the build agents do
+ * not directly check out the source code and therefore may not have access to
+ * the git repository information, it is possible to specific a system property
+ * that supersedes checking the file system for the currently checked out
+ * branch. Users can pass in the system property, <b>testngit.currentBranch</b>,
+ * to specify the name of the current branch instead of attempting to read the
+ * information for the local Git repository.
+ * 
  * @author akutz
  * 
  */
@@ -45,41 +55,31 @@ public class TestNGAnnotationBranchEnabler implements IAnnotationTransformer
         @SuppressWarnings("rawtypes") Constructor testConstructor,
         Method testMethod)
     {
+        String testName =
+            testMethod != null ? testMethod.getName() : testClass
+                .getSimpleName();
+
+        log.info("transforming test annotation f/ '{}'", testName);
+
         if (!annotation.getEnabled())
         {
-            log.info("test is already disabled; not inspecting branch");
+            log.info(
+                "test='{}' is already disabled; not inspecting branch",
+                testName);
             return;
         }
 
-        // Get the name of the currently checked out branch.
-        FileRepositoryBuilder gitBuilder = new FileRepositoryBuilder();
-        Repository repo;
-
-        try
+        String branchName = getCurrentBranch();
+        if (branchName == null)
         {
-            repo = gitBuilder.findGitDir().build();
-        }
-        catch (IOException e)
-        {
-            log.error("error finding git directory", e);
+            log.error("unable to read current branch");
             return;
         }
-
-        String branchName;
-
-        try
-        {
-            branchName = repo.getBranch();
-            log.info("currently checked out branch='{}'", branchName);
-        }
-        catch (IOException e)
-        {
-            log.error("error getting branch name", e);
-            return;
-        }
+        
+        log.info("currently checked out branch='{}'", branchName);
 
         String[] groups = annotation.getGroups();
-        log.info("test annotation groups={}", (Object) groups);
+        log.info("annotation groups={} f/ test='{}'", groups, testName);
 
         String itBranchesStr = System.getProperty("testngit.itbranches", null);
         log.info("testng.itbranches='{}'", itBranchesStr);
@@ -94,10 +94,60 @@ public class TestNGAnnotationBranchEnabler implements IAnnotationTransformer
             new ArrayList<String>((groups == null ? 0 : groups.length)
                 + (itBranches == null ? 0 : itBranches.length));
 
+        if (groups != null)
+        {
+            validBranches.addAll(Arrays.asList(groups));
+        }
+
+        if (itBranches != null)
+        {
+            validBranches.addAll(Arrays.asList(itBranches));
+        }
+
         if (!validBranches.contains(branchName))
         {
-            log.info("no group or itbranch matched current branch; disabled");
+            log.info("disabled test='{}'", testName);
             annotation.setEnabled(false);
         }
+    }
+
+    private String getCurrentBranch()
+    {
+        String branchName = System.getProperty("testngit.currentBranch", null);
+
+        if (branchName != null)
+        {
+            log.info(
+                "read branch name='{}' f/ sysprop 'testngit.currentBranch'",
+                branchName);
+            return branchName;
+        }
+
+        // Get the name of the currently checked out branch.
+        FileRepositoryBuilder gitBuilder = new FileRepositoryBuilder();
+        Repository repo;
+
+        try
+        {
+            repo = gitBuilder.findGitDir().build();
+        }
+        catch (IOException e)
+        {
+            log.error("error finding git directory", e);
+            return null;
+        }
+
+        try
+        {
+            branchName = repo.getBranch();
+            log.info("currently checked out branch='{}'", branchName);
+        }
+        catch (IOException e)
+        {
+            log.error("error getting branch name", e);
+            return null;
+        }
+
+        return branchName;
     }
 }
